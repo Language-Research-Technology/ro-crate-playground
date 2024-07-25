7093c77ad0dd92bc35f9d434a02d3a00e6f53187
Moises Sacal <moisbo@gmail.com>
2024-07-25 17:29:28 +1000

    updated link

--- ui/src/views/AboutView.vue
+++ ui/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This project is backed by LDaCA and its members. For more please visit <a href="www.ldaca.edu.au">www.ldaca.edu.au</a> </h1>
+    <h1>This project is backed by LDaCA and its members. For more please visit <a href="https://www.ldaca.edu.au">www.ldaca.edu.au</a> </h1>
   </div>
 </template>
 
