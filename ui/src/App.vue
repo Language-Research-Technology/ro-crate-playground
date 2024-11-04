6bdf5ae4f2b848408108b69f7052073a44fea2b9
Moises Sacal <moisbo@gmail.com>
2024-11-04 10:14:14 +1000

    changing where the valdiation is and adding tour

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -20,13 +20,13 @@ onMounted(() => {
 <template>
   <header class="sticky top-0 z-50">
     <el-row :gutter="0" :offset="0" style="" class="flex items-center justify-center">
-      <el-col :xs="24" :sm="24" :md="24" :lg="22" :xl="17" :offset="0">
+      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" :offset="0">
         <Navigation/>
       </el-col>
     </el-row>
   </header>
   <el-row :gutter="0" :offset="0" style="" class="flex items-center justify-center">
-    <el-col :xs="24" :sm="24" :md="24" :lg="22" :xl="17" :offset="0">
+    <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" :offset="0">
       <main class="">
         <RouterView/>
       </main>
--- ui/src/views/ExplorerView.vue
+++ ui/src/views/ExplorerView.vue
@@ -6,10 +6,13 @@ import {
   CopyDocument,
   HomeFilled,
   Box, Files,
-  CloseBold
+  CloseBold,
+  CircleCheck,
+  Warning,
+  WarnTriangleFilled, QuestionFilled, List
 } from '@element-plus/icons-vue'
 import {onMounted, onUpdated, reactive, ref, toRaw} from 'vue';
-import mode from 'ro-crate-modes/modes/default.json';
+import mode from 'ro-crate-modes/modes/base.json';
 import {ROCrate, validate as validateCrate} from "ro-crate";
 //import {Validator} from "ro-crate/lib/validator";
 import {Preview} from 'ro-crate-html';
@@ -24,20 +27,28 @@ import Upload from "@/components/Upload.vue";
 import JsonEditorVue from 'json-editor-vue'
 import {useStore} from "@/stores/store.js";
 import rocratelogo from '@/assets/ro-crate-logo.png';
-import {ElMessage, ElMessageBox} from 'element-plus'
+import {ElMessage, ElMessageBox} from 'element-plus';
 import HelpView from "@/views/HelpView.vue";
 import {useGtag} from 'vue-gtag-next';
 
 const store = useStore();
 const gtag = useGtag();
 
+const tour1 = ref();
+const tour2 = ref();
+const tour3 = ref();
+const tour4 = ref();
+const tour5 = ref();
+
+let validIcon = ref('success');
+
 const activeIndex = ref('1');
 let menu = reactive({main: '-'});
 const drawer = ref(false);
 const status = reactive({
   htmlPreviewLoading: false
-})
-const value = ref();
+});
+
 const validation = reactive({
   crate: null,
   context: {
@@ -46,7 +57,8 @@ const validation = reactive({
   },
   hasRootDataset: null,
   generalError: false,
-  running: false
+  running: false,
+  error: null
 });
 
 const valid = [
@@ -58,14 +70,23 @@ const valid = [
 
 const explorer = {
   files: [],
-  crate: {},
+  crate: null,
   json: {},
   refresh: () => {
     console.log('refresh')
   }
 };
 
-function ready(roc, refresh) {
+let openTour = ref(false);
+
+store.$subscribe((mutation, state) => {
+  if (mutation.events.key === 'crate') {
+    console.log(`Crate changed! ${state.crate}`);
+    validIcon.value = 'reValidate';
+  }
+});
+
+async function ready(roc, refresh) {
   explorer.crate = roc;
   explorer.refresh = refresh;
 }
@@ -74,19 +95,21 @@ const handleSelect = async (key, keyPath) => {
   console.log('handleSelect');
   console.log(key, keyPath)
   menu.show = key;
-  if (key === 'crate') {
-    crateViewActive.value = 'json';
-    await validate();
+  // if (key === 'crate') {
+  //   crateViewActive.value = 'json';
+  //   await validate();
+  // }
+  if (key === 'help') {
+    openTour.value = true;
   }
   console.log(menu.show)
 }
 const handleOpen = async (key, keyPath) => {
   console.log('handleOpen');
   //console.log(key, keyPath);
-  menu.show = key;
+  //menu.show = key;
   if (key === 'crate') {
     crateViewActive.value = 'json';
-    await validate();
   }
   console.log(menu.show)
 }
@@ -104,19 +127,39 @@ onMounted(async () => {
   ]
 });
 
-onUpdated(async () => {
-  await validate();
-})
+// onUpdated(async () => {
+//   await validate();
+// });
+
 
 const validate = async () => {
-  validation.running = true;
-  validation.results = [];
-  const results = await validateCrate(store.crate);
-  validation.results = results.slice();
-  validation.running = false;
+  console.log(explorer.crate)
+  if(explorer.crate) {
+    validation.running = true;
+    validation.results = [];
+    try {
+      store.crate = explorer.crate.toJSON();
+      const results = await validateCrate(store.crate);
+      validation.results = results.slice();
+      validIcon.value = 'success';
+      for (let valid of validation.results) {
+        if (valid.status === 'error') {
+          validIcon.value = 'error';
+        }
+        if (valid.status === 'warning') {
+          validIcon.value = 'warning';
+        }
+      }
+    } catch (e) {
+      validation.results = [];
+      validation.generalError = e.message;
+    }
+    validation.running = false;
+  }
 }
 const crateViewActive = ref('first');
 const switchCrateView = async (e) => {
+
   if (e.paneName === 'json') {
     store.crate = explorer.crate.toJSON();
   }
@@ -124,7 +167,6 @@ const switchCrateView = async (e) => {
     store.crate = explorer.crate.toJSON();
     await generateHTMLPreview();
   }
-  await validate();
 }
 
 
@@ -144,7 +186,6 @@ const setCrate = async ({example, upload}) => {
     }
     menu.show = 'crate';
     crateViewActive.value = 'json';
-    await validate();
   } else {
     ElMessageBox.confirm(
         'This will replace your current crate. Continue?',
@@ -164,7 +205,6 @@ const setCrate = async ({example, upload}) => {
       }
       menu.show = 'crate';
       crateViewActive.value = 'json';
-      await validate();
       ElMessage({
         type: 'success',
         message: 'Create New completed',
@@ -213,23 +253,23 @@ const fileUploaded = async (data) => {
 
 <template>
   <el-row :gutter="10">
-    <el-col :xs="24" :sm="4" :md="6" :lg="4" :xl="4">
+    <el-col :xs="24" :sm="4" :md="4" :lg="4" :xl="4">
       <el-menu
           :default-active="'home'"
-          class="el-menu-demo"
+          class="el-menu-explorer"
           mode="vertical"
           @select="handleSelect"
           :router="false"
       >
-        <el-menu-item index="home" collapse="false">
+        <el-menu-item index="home" collapse="false" ref="tour1">
           <template #title>
             <el-icon>
               <home-filled/>
             </el-icon>
-            <span>Home</span>
+            <span>Upload Crate</span>
           </template>
         </el-menu-item>
-        <el-menu-item index="examples">
+        <el-menu-item index="examples" ref="tour4">
           <template #title>
             <el-icon>
               <CopyDocument/>
@@ -237,23 +277,58 @@ const fileUploaded = async (data) => {
             <span>Examples</span>
           </template>
         </el-menu-item>
-        <el-menu-item index="help">
-          <template #title>
-            <el-icon>
-              <help/>
-            </el-icon>
-            <span>Help</span>
-          </template>
-        </el-menu-item>
         <hr/>
         <template v-if="!crateIsEmpty()">
-          <div class="text-2xl p-4 px-6 flex items-center justify-center">
+          <div class="text-2xl p-4 px-6 flex items-center justify-center"
+               ref="tour3">
             <el-icon>
               <Box/>
             </el-icon>
             <span class="px-2">Crate</span>
           </div>
-          <el-menu-item index="crate">ro-crate-metadata.json</el-menu-item>
+          <el-menu-item index="crate">
+            <el-tooltip v-if="validIcon === 'reValidate'"
+                        class="box-item"
+                        effect="dark"
+                        content="Click to validate crate"
+                        placement="top"
+            >
+              <el-icon size="30" @click="drawer = true; validate()" style="margin-right: 3px;">
+                <List />
+              </el-icon>
+            </el-tooltip>
+            <el-tooltip v-if="validIcon === 'success'"
+                        class="box-item"
+                        effect="dark"
+                        content="Valid crate"
+                        placement="top"
+            >
+              <el-icon size="30" @click="drawer = true; validate()" style="margin-right: 3px;color: darkgreen">
+                <CircleCheck/>
+              </el-icon>
+            </el-tooltip>
+            <el-tooltip v-if="validIcon === 'warning'"
+                        class="box-item"
+                        effect="dark"
+                        content="There are some warnings in your crate, click to see the detail"
+                        placement="top"
+            >
+              <el-icon size="30" @click="drawer = true;  validate()" style="margin-right: 3px;color: gold">
+                <Warning/>
+              </el-icon>
+            </el-tooltip>
+            <el-tooltip v-if="validIcon === 'error'"
+                        class="box-item"
+                        effect="dark"
+                        content="There are some errors in your crate, click to see the detail"
+                        placement="top"
+            >
+              <el-icon size="30" @click="drawer = true; validate()" style="margin-right: 3px;color: red">
+                <WarnTriangleFilled/>
+              </el-icon>
+            </el-tooltip>
+            <p ref="tour2">ro-crate-metadata.json</p>
+          </el-menu-item>
           <div>
             <div class="text-1xl p-4 px-6 flex items-center justify-center">
               <el-icon>
@@ -278,18 +353,24 @@ const fileUploaded = async (data) => {
             </el-menu-item>
             <hr/>
             <div class="text-2xl p-4 px-6 flex items-center justify-center">
-              <el-button type="primary" disabled @click="()=>{}">Add Files</el-button>
+              <el-button type="primary" disabled @click="()=>{}"
+                         ref="tour6">Add Files</el-button>
             </div>
           </div>
         </template>
         <div class="text-2xl p-4 px-6 flex items-center justify-center">
           <el-button type="primary" color="brown" @click="setCrate({})"
-                     class="custom-button">Create New
+                     class="custom-button" ref="tour5">Create New
           </el-button>
+        </div>
+        <div class="text-2xl p-4 px-6 flex items-center justify-center">
+          <el-icon @click="openTour = true">
+            <QuestionFilled/>
+          </el-icon>
         </div>
       </el-menu>
     </el-col>
-    <el-col :xs="24" :sm="16" :md="14" :lg="14" :xl="14">
+    <el-col :xs="24" :sm="20" :md="20" :lg="20" :xl="20">
       <div v-if="menu.show==='home'">
         <div class="flex items-center justify-center">
           <div class="h-32 w-[600px]">
@@ -371,36 +452,68 @@ const fileUploaded = async (data) => {
         </div>
       </div>
       <div v-if="menu.show==='help'">
-        <HelpView/>
       </div>
     </el-col>
-    <el-col :xs="24" :sm="16" :md="6" :lg="6" :xl="6">
-      <el-container v-if="menu.show==='crate'">
-        <el-header>
-          <div class="text-2xl flex items-center justify-center">
-            <span class="">
-              <el-button @click="validate()">Validate</el-button>
-            </span>
-          </div>
-        </el-header>
-        <el-main v-loading="validation.running">
-          <hr/>
-          <div v-for="item of validation.results" class="m-1">
-            <el-alert :type="item?.status" :closable="false">
-              <p>
+  </el-row>
+  <el-drawer
+      v-model="drawer"
+      title="RO-Crate Validation"
+      direction="rtl"
+      size="30%"
+  >
+    <el-container>
+      <el-header>
+        <div class="text-2xl flex items-center justify-center">
+          Basic Validation for your RO-Crate
+        </div>
+      </el-header>
+      <el-main v-loading="validation.running">
+        <div v-for="item of validation.results" class="m-1">
+          <el-alert :type="item?.status" :closable="false">
+            <p>
                 <span v-if="item?.status === 'error'">
                   <el-icon><CloseBold class="red-600"/></el-icon>
                 </span>
-                <span class="font-bold uppercase">{{ item?.status }}: </span>
-                <span v-if="item?.message">{{ item?.message }}</span>
-                <span v-else>{{ item?.clause }}</span>
-              </p>
-            </el-alert>
-          </div>
-        </el-main>
-      </el-container>
-    </el-col>
-  </el-row>
+              <span class="font-bold uppercase">{{ item?.status }}: </span>
+              <span v-if="item?.message">{{ item?.message }}</span>
+              <span v-else>{{ item?.clause }}</span>
+            </p>
+          </el-alert>
+        </div>
+        <div v-if="validation.generalError"
+             class="text-2xl">
+          General Error: {{ validation.generalError }}
+        </div>
+      </el-main>
+    </el-container>
+  </el-drawer>
+  <el-tour v-model="openTour" type="primary" :mask="false">
+    <el-tour-step
+        :target="tour1?.$el"
+        title="Upload RO-Crate"
+        description="If you have already an ro-crate-metadata.json file click here to upload it, if you dont have one click 'Create New'"
+    />
+    <el-tour-step
+        :target="tour2?.$el"
+        title="Edit ro-crate-metadata.json"
+        description="In this pane you can edit your ro-crate-metadata file with a plain json editor or with a Visual Editor Ro-Crate. An HTML preview will be generated everytime you switch tabs."
+    />
+    <el-tour-step
+        :target="tour3?.$el"
+        title="Validate your ro-crate-metadata.json"
+        description="This button will run simple validation for your ro-crate according to the spec"
+    />
+    <el-tour-step
+        :target="tour4?.$el"
+        title="Load Examples"
+        description="Click to show examples of RO-Crates to play around!"
+    />
+    <el-tour-step
+        :target="tour5?.$el"
+        title="Create New"
+        description="Create a new RO-Crate from a base ro-crate"
+    />
+  </el-tour>
 </template>
 <style>
 .custom-button {
@@ -409,5 +522,15 @@ const fileUploaded = async (data) => {
 
 .text-editor-json {
   height: calc(100vh - 4rem); /* Adjust 4rem to match the header height */
+}
+
+.el-menu-explorer {
+  white-space: nowrap;
+  font-size: 1rem; /* Adjust font size */
+  padding: 8px 16px; /* Adjust padding as necessary */
+}
+
+.el-menu-item > p {
+  overflow: scroll;
 }
 </style>
